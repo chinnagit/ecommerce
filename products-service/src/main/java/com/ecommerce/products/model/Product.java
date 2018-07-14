b818a8adcfcd16b55b6f6f77194173e7583b4296
chinnapapannagari, balakrishna <balakrishna.chinnapapannagari@emc.com>
2018-07-13 17:23:12 -0700

    to run locally

--- products-service/src/main/java/com/ecommerce/products/model/Product.java
+++ products-service/src/main/java/com/ecommerce/products/model/Product.java
@@ -14,59 +14,79 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * @author chinnb
  *
  */
-@Document(collection="products")
-public class Product implements Serializable{
-	private static final long serialVersionUID = 1L;
-	@Id
-	private int id;
-	@Indexed
-	String name;
-	@Indexed
-	String category;
-	BigDecimal price;
-	String description;
-	
-	public Product() {
-		
-	}
-	public Product(String name, String category, BigDecimal price, String description) {
-		this.name = name;
-		this.category = category;
-		this.price = price;
-		this.description = description;
-	}
-	
-	public int getId() {
-		return id;
-	}
+@Document(collection = "products")
+public class Product implements Serializable {
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    private static final long serialVersionUID = 1L;
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public BigDecimal getPrice() {
-		return price;
-	}
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    @Id
+    private int id;
+
+    @Indexed
+    String name;
+
+    @Indexed
+    String category;
+
+    BigDecimal price;
+
+    String description;
+
+    public Product() {
+
+    }
+
+    public Product(int id, String name, String category, BigDecimal price, String description) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", category=" + category + ", price=" + price + ", description=" + description + "]";
+    }
 
 }
