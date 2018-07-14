6cc0e9918e1ce4bd3590e97125e0b40dc348563e
chinnapapannagari, balakrishna <balakrishna.chinnapapannagari@emc.com>
2018-07-13 17:18:15 -0700

    to run locally

--- client/ecommerce-ui-jwt/src/app/app.module.ts
+++ client/ecommerce-ui-jwt/src/app/app.module.ts
@@ -5,26 +5,84 @@ import { HttpModule } from '@angular/http';
 import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login.component';
-import { HomeComponent } from './home.component';
-import { FooComponent } from './foo.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './commerce/home.component';
+import { MainComponent } from './commerce/main.component';
+import { ElectronicsComponent } from './commerce/electronics.component';
+import {ClothingComponent} from './commerce/clothing.component';
+import {ShoesComponent} from './commerce/shoes.component';
+import {CartComponent} from './commerce/cart.component';
+//import { FooComponent } from './commerce/foo.component';
+import { ClarityModule } from '@clr/angular';
+import { SESSION_STORAGE, StorageServiceModule, WebStorageService} from 'angular-webstorage-service';
+import {CART_SERVICE_STORAGE, CartService} from './commerce/cart.service';
+import {CheckOutComponent} from './commerce/checkout.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    FooComponent    
+    // FooComponent,
+    MainComponent,
+    ElectronicsComponent,
+    ClothingComponent,
+    ShoesComponent,
+    CartComponent,
+    CheckOutComponent
   ],
   imports: [
     BrowserModule,
+    StorageServiceModule,
     FormsModule,
     HttpModule,
+      ClarityModule,
     RouterModule.forRoot([
-     { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent }])
+
+      { path: 'home', component: HomeComponent },
+      {
+        path: '',
+        component: HomeComponent,
+          children: [
+              {
+                  path: '', redirectTo: 'main', pathMatch: 'full'
+              },
+              {
+                  path: 'main',
+                  component: MainComponent,
+                  children: [
+                      {
+                          path: '', redirectTo: 'electronics', pathMatch: 'full'
+                      },
+                      {
+                          path: 'electronics',
+                          component: ElectronicsComponent
+                      },
+                      {
+                          path: 'clothing',
+                          component: ClothingComponent
+                      },
+                      {
+                          path: 'shoes',
+                          component: ShoesComponent
+                      },
+                      {
+                          path: 'cart',
+                          component: CartComponent
+                      },
+                      {
+                          path: 'checkout',
+                          component: CheckOutComponent
+                      }
+                  ]
+              },
+          ]
+      },
+      { path: 'login', component: LoginComponent }])
   ],
-  providers: [],
+  providers: [{ provide: CART_SERVICE_STORAGE, useExisting: SESSION_STORAGE },
+      CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
