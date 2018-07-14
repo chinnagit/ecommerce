ce6bd2481cafe208a3bbdb561a92dea58c7fad96
chinnapapannagari, balakrishna <balakrishna.chinnapapannagari@emc.com>
2018-07-13 17:18:37 -0700

    to run locally

--- client/ecommerce-ui-jwt/src/app/app.service.ts
+++ client/ecommerce-ui-jwt/src/app/app.service.ts
@@ -16,33 +16,6 @@ export class Foo {
 export class AppService {
   constructor(
     private _router: Router, private _http: Http){}
- 
-  obtainAccessToken(loginData){
-    let params = new URLSearchParams();
-    params.append('username',loginData.username);
-    params.append('password',loginData.password);    
-    params.append('grant_type','password');
-    params.append('client_id','fooClientIdPassword');
-
-    let headers = new Headers({'Content-type': 'application/x-www-form-urlencoded; charset=utf-8', 'Authorization': 'Basic '+btoa("fooClientIdPassword:secret")});
-    let options = new RequestOptions({ headers: headers });
-    console.log(params.toString());
-     this._http.post('http://localhost:9081/security-oauth-server/oauth/token', params.toString(), options)
-    .map(res => res.json())
-    .subscribe(
-      data => this.saveToken(data),
-      err => alert('Invalid Credentials')
-    ); 
-  }
-
-
-  saveToken(token){
-    var expireDate = new Date().getTime() + (100000 * token.expires_in);
-    Cookie.set("access_token", token.access_token, expireDate);
-    console.log('Obtained Access token');
-    this._router.navigate(['/']);
-  }
- 
 
   getResource(resourceUrl) : Observable<Foo>{
 	console.log("access_token: \n"+Cookie.get('access_token'));
@@ -66,15 +39,4 @@ export class AppService {
 	  console.log(errMsg);
 	  return Observable.throw(error);
 	}
-
-  checkCredentials(){
-    if (Cookie.get('access_token') == 'undefined' || !Cookie.check('access_token')){
-        this._router.navigate(['/login']);
-    }
-  } 
-
-  logout() {
-    Cookie.delete('access_token');
-    this._router.navigate(['/login']);
-  }
 }
